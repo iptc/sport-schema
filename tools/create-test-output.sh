b71c5dad96e2c881ac1505a8a946e8fa9e413d66
Brendan Quinn <brendan@cluefulmedia.com>
2022-02-22 11:32:42 +0200

    Changing test scripts so we can run queries using the ontology file

--- tools/create-test-output.sh
+++ tools/create-test-output.sh
@@ -6,9 +6,12 @@
 # queries are updated.
 
 TTL_SAMPLES_DIR='samples/ttl'
+SPORT_ONTOLOGY_FILE='ontologies/iptc-sport-ontology.ttl'
+REGEX_FOR_VALID_FILES='conforms  *true'
 
 # build list of data files to use in sparql queries
-DATA_STRING=''
+# start with the ontology file so it can also be used in test queries
+DATA_STRING="--data ${SPORT_ONTOLOGY_FILE}"
 for filename in ${TTL_SAMPLES_DIR}/*.ttl; do
     DATA_STRING="${DATA_STRING} --data=$filename"
 done
@@ -16,5 +19,6 @@ done
 for queryfile in queries/*.rq; do
     name=${queryfile##*/}  # get part after last /
     base=${name%.rq}     # strip off .xml extension
+    echo "Creating output file for ${base}..."
     sparql ${DATA_STRING} --query=$queryfile >queries/output/$base.out
 done
